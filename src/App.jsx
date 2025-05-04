64cc533d9b089a451ab52c764c526e09cb02afd5
Matheus Brito <ultrapistol@gmail.com>
2025-05-04 16:58:30 -0300

    inicio site

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import Navbar from './components/NavBar/navbar';
+
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+            <Navbar/>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/NavBar/navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div>navbar</div>
+  )
+}
+
+export default Navbar
