d91ebf47bd632a5e3d5b4b1caf5da2d3b661cbfa
Matheus Brito <ultrapistol@gmail.com>
2025-05-05 22:22:34 -0300

    navbar creation

--- src/components/NavBar/navbar.jsx
+++ src/components/NavBar/navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './navbar.css';
+import logo from '../../assets/JRMlogo.png';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className="navbar">
+      <img src={logo} alt="Logo" className='logo'/>
+      <div className="desktopMenu">
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className="desktopMenuListItem">Serviços</Link>
+        <Link className="desktopMenuListItem">Sobre</Link>
+        <Link className="desktopMenuListItem">AValiações</Link>
+
+      </div>
+      <button className="desktopMenuBtn">
+          <img src="" alt="" className="desktopMenuImg" />Contato</button>
+    </nav>
   )
 }
 
